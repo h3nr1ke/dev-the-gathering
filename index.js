c1b5141445bebca3096b8105aba6bf87e796fee4
Francisco Deodato <francisco.deodato@tvfsoftware.com>
2019-02-26 20:28:40 -0300

    added database initial configuration

--- index.js
+++ index.js
@@ -3,15 +3,20 @@
  * @author Henrique Deodato <devthegathering@apps.etc.br>
  */
 
-var http = require('http');
+const http = require('http');
 const express = require('express');
 const app = express();
-var logger = require('morgan');
+const logger = require('morgan');
 const helmet = require('helmet');
+const bodyParser= require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+
+// load the data from env vars
+require('dotenv-safe').load();
 
 // ENDPOINTS
-app.get('/test', (req, res, next) => {
-    console.log("/test");
+app.get('/login', (req, res, next) => {
+    console.log("/login");
     res.status(200).send('/test');
 });
 
@@ -22,4 +27,12 @@ app.use(express.urlencoded({ extended: false }));
 
 // create the server and start listening
 var server = http.createServer(app);
-server.listen(3000);
+
+// connects do the database and create a global var to share the connection
+MongoClient.connect( process.env.MONGO_CONNECTION, (err, database) => {
+    if (err) return console.log(err)
+    global.db = database.db( process.env.MONGO_DB ) // database name
+    
+    // since we have a database connection, start the server...
+    server.listen(3000);
+});
